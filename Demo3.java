a6a2a2675c70984d8e66dea190853e7875d12a7f
Mohammed Fawaz <127772058+fawaaaaz@users.noreply.github.com>
2024-08-29 11:11:39 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,13 @@
+package com.verizon;
+import java.util.Scanner;
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name= sc.nextLine();
+		System.out.println("Your name is:" +name);
+	}
+
+}
